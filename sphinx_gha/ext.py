f30964fbe0e3f74a9c96e97cf85d6d7cf5094012
Andrew Cassidy <drewcassidy@me.com>
2024-09-25 23:12:52 -0700

    custom directive for items

--- sphinx_gha/ext.py
+++ sphinx_gha/ext.py
@@ -5,13 +5,14 @@ from pathlib import Path
 import yaml
 from docutils import nodes
 from docutils.nodes import Element
-from docutils.parsers.rst import directives
+from docutils.parsers.rst import directives, Directive
 from myst_parser.mdit_to_docutils.base import DocutilsRenderer
 from myst_parser.mdit_to_docutils.sphinx_ import SphinxRenderer
-from myst_parser.mocking import MockInliner
+from myst_parser.mocking import MockInliner, MockStateMachine, MockState
 from myst_parser.parsers.mdit import create_md_parser
 from myst_parser.parsers.sphinx_ import MystParser
 from sphinx import application
+from sphinx.addnodes import versionmodified
 from sphinx.domains.std import ConfigurationValue
 from sphinx.util.docutils import SphinxDirective, new_document
 
@@ -26,34 +27,45 @@ def indent(text: str, level=1):
     return '\n'.join([(' ' * level) + line for line in lines])
 
 
-class MarkdownParsingMixin(Element):
-    def __init__(self, rawsource: str = "", *children, **attributes):
-        super().__init__(rawsource, *children, **attributes)
-
-        self.md_parser = None
-        self.md_renderer = None
-
-    def parse_markdown_inline(self, markdown):
-        if not hasattr(self, 'md_renderer'):
+class MarkdownParsingMixin(Directive):
+    @property
+    def md_renderer(self):
+        if not hasattr(self, '_md_renderer'):
             config = self.state.document.settings.env.myst_config
-            self.md_parser = create_md_parser(config, SphinxRenderer)
-            self.md_renderer = DocutilsRenderer(self.md_parser)
-            self.md_renderer.setup_render({"myst_config": config}, {})
-        inliner = MockInliner(self.md_renderer)
-        return inliner.parse(markdown, self.lineno, None, None)[0]
+            self._md_parser = create_md_parser(config, SphinxRenderer)
+            self._md_renderer = DocutilsRenderer(self._md_parser)
+            self._md_renderer.setup_render({"myst_config": config}, {})
+        return self._md_renderer
+
+    def parse_markdown(self, markdown, inline=False, node=None):
+        renderer = self.md_renderer
+        renderer.current_node = node or Element('')
+        renderer.nested_render_text(markdown, self.lineno, inline=inline)
+        return renderer.current_node.children
 
 
-class ActionInputDirective(ConfigurationValue):
+class ActionItemDirective(ConfigurationValue, MarkdownParsingMixin):
+    _name = 'gh-action-item'
     option_spec = ConfigurationValue.option_spec | {
         'required': directives.unchanged_required,
         'deprecationMessage': directives.unchanged_required
     }
 
+    @classmethod
+    def generate(cls, item_name, item_meta, lineno, content_offset, state, state_machine):
+        options = {k: str(v) for k, v in item_meta.items() if k in cls.option_spec}
+        directive = cls('confval', [item_name], options, '', lineno, content_offset, "", state, state_machine)
+        node = directive.run()
+        if 'description' in item_meta:
+            directive.parse_markdown(item_meta['description'], node=node[1][1])
+        return node
+
     def format_deprecationMessage(self, message):
-        document = new_document('', self.state.document.settings)
-        parser = MystParser()
-        parser.parse(message, document)
-        parsed = document.children
+        admonition = versionmodified()
+        admonition['type'] = 'deprecated'
+        admonition.document = self.state.document
+        self.parse_markdown(message, inline=True, node=admonition)
+        return admonition, []
 
     def format_required(self, required: str):
         """Formats the ``:type:`` option."""
@@ -72,13 +84,13 @@ class ActionInputDirective(ConfigurationValue):
             field_list.append(field)
             field_list += msgs
         if 'required' in self.options:
-            field, msgs = self.format_type(self.options['required'])
+            field, msgs = self.format_required(self.options['required'])
             field_list.append(field)
             field_list += msgs
         if len(field_list.children) > 0:
             content_node.insert(0, field_list)
 
-        super(ActionInputDirective, self).transform_content(content_node)
+        super(ActionItemDirective, self).transform_content(content_node)
 
 
 class ActionDirective(SphinxDirective, MarkdownParsingMixin):
@@ -117,18 +129,19 @@ class ActionDirective(SphinxDirective, MarkdownParsingMixin):
             for item_name, item_meta in items.items():
                 if item_meta is None:
                     item_meta = {}
-                item_rst = [f'.. {directive} :: {item_name}']
-                for meta_tag in metas:
-                    if meta_tag in item_meta:
-                        item_rst.append(indent(f':{meta_tag}: {item_meta[meta_tag]}'))
-                item_nodes = self.parse_text_to_nodes('\n'.join(item_rst))
-                if 'description' in item_meta:
-                    item_nodes[1][1].extend(self.parse_markdown_inline(item_meta['description']))
-                value_section.extend(item_nodes)
+                value_section.extend(ActionItemDirective.generate(item_name, item_meta, self.lineno, self.content_offset, self.state, self.state_machine))
+                # item_rst = [f'.. {directive} :: {item_name}']
+                # for meta_tag in metas:
+                #     if meta_tag in item_meta:
+                #         item_rst.append(indent(f':{meta_tag}: {item_meta[meta_tag]}'))
+                # item_nodes = self.parse_text_to_nodes('\n'.join(item_rst))
+                # if 'description' in item_meta:
+                #     item_nodes[1][1].extend(self.parse_markdown(item_meta['description']))
+                # value_section.extend(item_nodes)
             section.append(value_section)
 
         if 'description' in action_yaml:
-            section.extend(self.parse_markdown_inline(action_yaml['description']))
+            section.extend(self.parse_markdown(action_yaml['description']))
 
         if 'x-env' in action_yaml:
             document_values(
@@ -158,6 +171,7 @@ class ActionDirective(SphinxDirective, MarkdownParsingMixin):
 
 def setup(app: application.Sphinx) -> ty.Dict[str, ty.Any]:
     app.add_directive("gh-action", ActionDirective)
+    app.add_directive('gh-action-item', ActionItemDirective)
     app.add_config_value('sphinx_gha_repo_tag', os.environ.get('READTHEDOCS_GIT_IDENTIFIER'), 'env')
     app.add_config_value('sphinx_gha_repo_slug', 'UNKNOWN REPO', 'env')
     app.add_config_value('sphinx_gha_repo_root', os.getcwd(), 'env')
